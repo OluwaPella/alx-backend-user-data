6e314d2e3ee1c6b5a5cf39955b36ec36adcb3e78
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-06 20:03:13 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,11 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
     GET /api/v1/unauthorized
     """
     abort(401)
-    
-    
