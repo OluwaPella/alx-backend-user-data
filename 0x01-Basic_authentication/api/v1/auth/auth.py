a967688a145ee27c4eea77329c06cab9a36b7cb3
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-07 15:46:54 +0100

    add: API authentication.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,7 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """logic that check if auth is required or not"""
-        if path is None:
-            return True
-        if excluded_paths is None or []:
+        if path is None or excluded_paths is None or len(excluded_paths) ==0:
             return True
         if path[-1] != "/":
             if not(path := path + '/'):
