411a55aed10428d6c7253196e9b88aa1d4e90860
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-07 14:51:51 +0100

    add: API authentication.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,20 +5,20 @@ from flask import request
 
 
 class Auth:
-    """doc doc"""
+    """logic for auth"""
 
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """doc doc"""
+        """logic that check if auth is required or not"""
         return False
     
 
     def authorization_header(self, request=None) -> str:
-        """ doc doc"""
+        """logic that check auth header"""
         return None
     
     
     def current_user(self, request=None) -> TypeVar('User'):
-        """doc doc"""
+        """logic that identify current_user"""
         return None
     
