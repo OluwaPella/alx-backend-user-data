14b257bc99a107492a45790f84816a60346fe4a6
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-07 15:50:29 +0100

    add: API authentication.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,7 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) ==0:
             return True
         if path[-1] != "/":
-            if not(path := path + '/'):
-                return False
+            path += "/"
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
