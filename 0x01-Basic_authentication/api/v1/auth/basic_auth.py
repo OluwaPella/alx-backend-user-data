76a61b962ce26edf4aa759f7dd93da8f932beeb3
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-09 17:36:53 +0100

    decode file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,15 +31,20 @@ class BasicAuth(Auth):
         except Exception:
             return None
         
-def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str)
-    """extract user credentials"""
-    if decoded_base64_authorization_header is None:
-        return None
-    if not isinstance(decoded_base64_authorization_header, str):
-        return None
-    if ':' not in decoded_base64_authorization_header:
-        return None
-    else:
-        credentials = decoded_base64_authorization_header.split(':', 1)
-        return (credentials[0], credentials[1])
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """extract user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None
+        if ':' not in decoded_base64_authorization_header:
+            return None
+        else:
+            credentials = decoded_base64_authorization_header.split(':', 1)
+            return credentials
+
+        
+    
+    
+
     
