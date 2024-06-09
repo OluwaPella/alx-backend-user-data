de5be28e2fe869f77d03e477094feb798043cc95
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-09 13:02:48 +0100

    add: implement on api Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,8 +11,8 @@ from api.v1.auth.auth import Auth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
+
 if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 
