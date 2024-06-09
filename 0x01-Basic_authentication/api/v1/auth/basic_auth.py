612b86441b1e4b2874f3acfc370fb912faa67659
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-09 15:21:34 +0100

    add:working

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,9 +8,10 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """extract base64 authorization header"""
         if authorization_header is None:
-            return None
-        if authorization_header is not str:
+             return None
+        if not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith('Basic '):
             return None
-        return authorization_header[6:]
+        else:
+            return authorization_header[6:]
