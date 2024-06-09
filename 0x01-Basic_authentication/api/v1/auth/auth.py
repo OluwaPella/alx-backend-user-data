1b33410811382d77aaf5fb13d6f94fcc4a8a7c85
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-09 13:37:46 +0100

    add: implement on api Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,8 +19,6 @@ class Auth:
         """logic that check auth header"""
         if request is None:
             return None
-        if request.headers.get('Authorization') is None:
-            return None
         return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
