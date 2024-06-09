29a159e442445cc2d60e52be7c9a68fd2baaa133
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-09 13:37:21 +0100

    add: implement on api Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,18 +16,6 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """an unauthorize function"""
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Error handler: Forbidden"""
-    return jsonify({"error": "Forbidden"}), 403
-
 @app.before_request
 def before_request():
     """doc doc"""
@@ -40,6 +28,19 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """an unauthorize function"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Error handler: Forbidden"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 
 
 if __name__ == "__main__":
