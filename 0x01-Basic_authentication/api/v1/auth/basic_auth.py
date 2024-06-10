0df5faa966f1004b38c781ddf1e7f5d6af94083e
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-10 02:19:24 +0100

    add:User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,18 +44,4 @@ class BasicAuth(Auth):
             return email, password
         except ValueError:
             return None
-        
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """user object from credentials"""
-        if user_email is None or not isinstance(user_email, str):
-            return None
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-        users = User.search({'email': user_email})
-        if not users:
-            return None
-        user = users[0]
-        if not user.is_valid_password(user_pwd):
-            return None
-        return user
-        
+
