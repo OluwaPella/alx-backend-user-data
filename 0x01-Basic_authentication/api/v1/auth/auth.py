d01d76ef0ac6480eb8ec3caa6d732a7be2e09e89
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-11 18:10:48 +0100

    add:added user validation'

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,7 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path[-1] != "/":
-            path += "/"
+            path += "/" and "*"
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
