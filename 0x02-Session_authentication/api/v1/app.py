fe0749dc34f9ff9c854e71f99385fcf76caad374
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 00:08:19 +0100

    add: updated code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,9 +31,10 @@ def before_requests():
     if auth is None:
         return
     if not auth.require_auth(request.path,
-                             ['/api/v1//status/',
-                              '/api/v1/unauthorized/',
-                              '/api/v1/forbidden/']):
+                              ['/api/v1/status/',
+                               '/api/v1/unauthorized/',
+                               '/api/v1/forbidden/',
+                               '/api/v1/auth_session/login/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
