3fc4156ba974d39545f23ecfe1ccc56103f3383b
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-13 13:29:11 +0100

    add:updated the users function

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -14,9 +14,9 @@ class Auth:
                 return True
         for excluded_path in excluded_paths:
              excluded_path =  excluded_path.strip()
-             
-             checked = ''
-             if excluded_path[-1] == '*':
+
+            checked = ''
+            if excluded_path[-1] == '*':
                 checked = '{}.*'.format(excluded_path[:-1])
             elif excluded_path[-1] == '/':
                 checked = '{}/*'.format(excluded_path[:-1])
@@ -24,7 +24,7 @@ class Auth:
                 checked = '{}/*'.format(excluded_path)
             if re.match(checked, path):
                 return False
-                    
+
         return True
 
     def authorization_header(self, request=None) -> str:
