3713c393e7b0733d0bea5761095ddb2d9380c2ba
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 20:33:50 +0100

    add session id

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,12 +51,12 @@ def before_requests():
 
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
 
-    if not auth.authorization_header(request) and auth.session_cookie(request):
-        abort(401)
 
 
 if __name__ == "__main__":
