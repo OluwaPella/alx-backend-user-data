fead1a44bf25287223fbcf55c63b7157ce58f08f
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 20:25:59 +0100

    add session id

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -48,16 +48,15 @@ def before_requests():
                               '/api/v1/forbidden/',
                               '/api/v1/auth_session/login/']):
         return
-    
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
-
 
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
+
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
 
 
 if __name__ == "__main__":
