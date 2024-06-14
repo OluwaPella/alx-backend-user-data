b7962568442b49d2e43d225b75c9c2a8c9e74793
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 18:00:38 +0100

    add session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """this instance method creates a Session ID for a user_id"""
+        """this instance method creates a Session ID for a user"""
         if user_id is None:
             return None
         if not isinstance(user_id, str):
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
             session_id = uuid.uuid4()
             self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """this instance method returns a User ID based on a Session ID"""
         if session_id is None:
@@ -26,4 +26,5 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         else:
-            return self.user_id_by_session_id.get(session_id)
+            return self.user_id_by_session_id.get(session_id, None)
+        
