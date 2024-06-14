9114dfebdff5ff134bdc21951b328ec15db4e95e
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 20:16:52 +0100

    add session id

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,6 +40,10 @@ def forbidden(error) -> str:
 @app.before_request
 def before_requests():
     """doc doc"""
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None
+    abort(401)
+
     if auth is None:
         return
     if not auth.require_auth(request.path,
@@ -54,9 +58,6 @@ def before_requests():
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None 
-    abort(401)
 
 
 if __name__ == "__main__":
