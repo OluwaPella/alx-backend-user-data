20c892c070ba28bbd824987acf503124af717a23
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 20:08:17 +0100

    add session id

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -12,9 +12,9 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """logic that check if auth is required or not"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
-                return True
+            return True
         for excluded_path in excluded_paths:
-            excluded_path =  excluded_path.strip()
+            excluded_path = excluded_path.strip()
             checked = ''
             if excluded_path[-1] == '*':
                 checked = '{}.*'.format(excluded_path[:-1])
@@ -23,8 +23,8 @@ class Auth:
             else:
                 checked = '{}/*'.format(excluded_path)
             if re.match(checked, path):
-             return False
-            
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
@@ -36,11 +36,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """logic that identify current_user"""
         return None
-    
+
     def session_cookie(self, request=None):
-        """return a cookies value from request"""
+        """retrieves  cookies value from request"""
         if request is None:
             return None
         session_name = os.getenv('SESSION_NAME')
         return request.cookies.get(session_name, None)
-    
