75b1c0f8aa69076e10c9ac891c258f0f8f3b620f
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 18:38:55 +0100

    add session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,6 +26,6 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         else:
-            user_iId = self.user_id_by_session_id.get[session_id]
-            return user_iId
+            users = self.user_id_by_session_id.get(session_id)
+            return users
         
