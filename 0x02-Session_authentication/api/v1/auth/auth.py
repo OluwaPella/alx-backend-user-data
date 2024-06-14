b48371d8884e0bc3b2448033572eaf02a790fbcc
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-14 15:39:04 +0100

    add:very good

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -13,16 +13,17 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
                 return True
         for excluded_path in excluded_paths:
-             excluded_path =  excluded_path.strip()
-             checked = ''
-             if excluded_path[-1] == '*':
-                 checked = '{}.*'.format(excluded_path[:-1])
-             elif excluded_path[-1] == '/':
-                 checked = '{}/*'.format(excluded_path[:-1])
-             else:
-                 checked = '{}*/'.format(excluded_path)
-                 if re.match(checked, path):
-                     return False
+            excluded_path =  excluded_path.strip()
+            checked = ''
+            if excluded_path[-1] == '*':
+                checked = '{}.*'.format(excluded_path[:-1])
+            elif excluded_path[-1] == '/':
+                checked = '{}/*'.format(excluded_path[:-1])
+            else:
+                checked = '{}/*'.format(excluded_path)
+            if re.match(checked, path):
+             return False
+            
         return True
 
     def authorization_header(self, request=None) -> str:
