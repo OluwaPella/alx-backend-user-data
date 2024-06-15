97aabf2f7df5693ccf542e17f38b50f1b58def1d
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-15 02:04:58 +0100

    add:updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,6 +57,7 @@ def before_requests():
     if auth.authorization_header(request) or auth.session_cookie(request):
         return None, abort(401)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
