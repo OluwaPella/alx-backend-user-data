e2cabd74dc88a9625d332ff9e8ada19a3dfca9dd
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-21 19:52:57 +0100

    add: New view for Session Authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/venv python3
+from flask import request, jsonify
+from models import User
+"""session auth"""
+@app.route('/api/v1/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login function"""
+    email = request.get('email')
+    password = request.get("password")
+    if email is None:
+        return jsonify({"error":"email missing"}), 400
+    if password is None:
+        return jsonify({"eeror": "password missing"}), 400
+    user = User.serach({"email": email})
+    if user is None:
+        return jsonify({"error": "password missing"})
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    return jsonify({"session_id": session_id})
+    
+    
