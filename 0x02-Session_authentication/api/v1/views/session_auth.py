621044c5a65031f8161af9938b7dcd63e3f20b75
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-23 22:25:39 +0100

    doc

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 """doc doc"""
 from flask import request, jsonify
-from models import User
-import os
-@app.route('/api/v1/auth_session/login', methods=['POST'], strict_slashes=False)
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """POST /api/v1/auth_session/login
-    Return:
-      - JSON representation of a User object.
-    """
-    email = request.get('email')
-    password = request.get("password")
-    if not email:
-        return jsonify({"error":"email missing"}), 400
-    if not password:
-        return jsonify({"eeror": "password missing"}), 400
-    user = User.serach({"email": email})
-    if  not user:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    user_data = user.to_json()
-    response = jsonify(user_data)
-    response.set_cookie(os.getenv["SESSION_NAME"], session_id, httponly=True)
-    return response
+    """this logic login and create session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or len(email) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if password is None or len(password) == 0:
+        return jsonify({"error": "password missing"})
+    
