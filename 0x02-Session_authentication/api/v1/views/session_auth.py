a5762f5085cf4173d3049c6ca0589e5f62eec95c
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-23 21:10:38 +0100

    doc

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,10 @@ from flask import request, jsonify
 from models import User
 from api.v1.app import auth, app
 import os
-"""session auth"""
+"""POST /api/v1/auth_session/login
+    Return:
+      - JSON representation of a User object.
+      """
 @app.route('/api/v1/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """doc doc"""
