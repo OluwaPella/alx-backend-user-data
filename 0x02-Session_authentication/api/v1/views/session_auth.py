d8eed4fca991320f041510f74e763064d319a592
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-06-29 16:52:30 +0100

    add: set cokkies

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,13 +2,11 @@
 """doc doc"""
 from flask import request, jsonify
 import os
-from typing import Tuple
 from api.v1.views import app_views
 from models.user import User
 
-
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> Tuple[str, int]:
+def login() -> str:
     """this logic login and create session"""
     email = request.form.get("email")
     password = request.form.get("password")
@@ -16,14 +14,14 @@ def login() -> Tuple[str, int]:
         return jsonify({"error": "email missing"}), 400
     if password is None or len(password) == 0:
         return jsonify({"error": "password missing"}), 400
-    user_data = User.search('email==email')
+    user_data = User.search(f'email="{email}"')
     if not user_data:
         return jsonify({"error": "no user found for this email"}), 404
     if user_data[0].is_valid_password(password):
         from api.v1.app import auth
         """create session_id"""
         sessiond_id = auth.create_session(getattr(user_data[0], 'id'))
-        user = jsonify(user_data[0].to_json())
+        user = user_data[0].to_json()
         user.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
         return user
     return jsonify({"error": "wrong password"}), 401
