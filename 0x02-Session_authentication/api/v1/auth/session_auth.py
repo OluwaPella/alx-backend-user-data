1de1304c4d3459ac296b02dc77a12f0f8d728475
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-07-01 15:42:56 +0100

    add

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -46,5 +46,7 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return False
+        
         del self.user_id_by_session_id[session_id]
+        
         return True
